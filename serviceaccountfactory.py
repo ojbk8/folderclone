1877a80a9053f7543edeeb3f5744a5b4f5388f9b
Mz <52611335+Mz49@users.noreply.github.com>
2019-09-04 19:44:18 -0700

    Revert "folderclone 0.2"

--- serviceaccountfactory.py
+++ serviceaccountfactory.py
@@ -1,17 +1,6 @@
 from google.oauth2.service_account import Credentials
-import googleapiclient.discovery, base64, json, progress.bar, glob, sys, argparse, time
+import googleapiclient.discovery, base64, json, progress.bar, glob, sys
 from os import mkdir
-
-stt = time.time()
-
-parse = argparse.ArgumentParser(description='A tool to create Google service accounts.')
-parse.add_argument('--path','-p',default='accounts',help='Specify an alternate directory to output the credential files.')
-parse.add_argument('--controller','-c',default='controller/*.json',help='Specify the relative path for the controller file.')
-parse.add_argument('--no-autofill',default=False,action='store_true',help='Do not autofill the first project.')
-
-args = parse.parse_args()
-acc_dir = args.path
-contrs = glob.glob(args.controller)
 
 def create_service_account_and_dump_key(project_id, service_account_name, service_account_filename):
 	
@@ -33,28 +22,25 @@ def create_service_account_and_dump_key(project_id, service_account_name, servic
 		}
 	).execute()
 	
-	f = open(service_account_filename, "w")
+	f = open("accounts/" + service_account_filename, "w")
 	f.write(base64.b64decode(key["privateKeyData"]).decode("utf-8"))
 	f.close()
 
+
+contrs = glob.glob('controller/*.json')
 try:
 	open(contrs[0],'r')
 	print('Found controllers.')
 except IndexError:
 	print('No controller found.')
 	sys.exit(0)
-
-proj = 0
-pid = 'pid'
+proj = 1
 projects = {}
 print('Add more projects:')
 print('[project id] [accounts to create]')
-if not args.no_autofill:
-	proj += 1
-	pid = json.loads(open(contrs[0],'r').read())['project_id']
-	projects[pid] = 99
-	print(str(proj) + '. ' + pid + ' 99')
-
+pid = json.loads(open(contrs[0],'r').read())['project_id']
+projects[pid] = 99
+print(str(proj) + '. ' + pid + ' 99')
 while pid != '':
 	proj += 1
 	pid = input(str(proj) + '. ')
@@ -63,14 +49,12 @@ while pid != '':
 		projects[a[0]] = a[1]
 
 prefix = ''
-
 while len(prefix) < 4:
 	prefix = input('Custom email prefix? ').lower()
 	if prefix == '':
 		prefix = 'folderclone'
 	if len(prefix) < 4:
 		print('Email prefix must be 5 characters or longer!')
-
 print('Using ' + str(len(projects)) + ' projects...')
 
 credentials = Credentials.from_service_account_file(contrs[0], scopes=[
@@ -79,7 +63,7 @@ credentials = Credentials.from_service_account_file(contrs[0], scopes=[
 iam = googleapiclient.discovery.build("iam", "v1", credentials=credentials)
 
 try:
-	mkdir(acc_dir)
+	mkdir('accounts')
 except FileExistsError:
 	pass
 
@@ -87,12 +71,10 @@ gc = 1
 for i in projects:
 	pbar = progress.bar.Bar("Creating accounts in %s" % i,max=int(projects[i]))
 	for o in range(1, int(projects[i]) + 1):
-		create_service_account_and_dump_key(i,prefix + str(o),acc_dir + "/" + str(gc) + '.json')
+		try:
+			create_service_account_and_dump_key(i,prefix + str(o),str(gc) + '.json')
+		except googleapiclient.errors.HttpError:
+			pass
 		gc += 1
 		pbar.next()
 	pbar.finish()
-
-print('Complete.')
-hours, rem = divmod((time.time() - stt),3600)
-minutes, sec = divmod(rem,60)
-print("Elapsed Time:\n{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),sec))
